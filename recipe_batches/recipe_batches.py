a8809df686b52c689b08a0e419170ca215f3416d
CookieMonsta89 <jlstan06@hotmail.com>
2019-03-01 09:16:58 -0500

    finished recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python
 
 import math
+import operator
 
 def recipe_batches(recipe, ingredients):
-  pass 
-
+  # Need to get the max value from ingredients.items, this is a starting point
+  batch = max(ingredients.items(), key=operator.itemgetter(1))[1]
+  # Now I need to loop 
+  for recKey in recipe.items():
+    # if the key from recipes does not appear in ingredients, batch=0 because we can't make recipe
+    if recKey[0] not in ingredients.keys():
+      batch = 0
+    # loop through the ingredients dict
+    for ingKey in ingredients.items():
+      # if the key of ingredients matches the key of the recipe
+      if ingKey[0] == recKey[0]:
+        # checking that we have more in ingrediants than the recipe calls for
+        if (ingKey[1] // recKey[1]) < batch:
+          # if it is lower, then we decrease the total number of batches we can make to this new total
+          batch = (ingKey[1] // recKey[1])
+  # return total number of batches we can make
+  return batch
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
