2930e657909178160c5fafeeda8377ac451aad42
CookieMonsta89 <jlstan06@hotmail.com>
2019-03-01 09:21:08 -0500

    adding climbing stairs without using cache

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,9 +2,19 @@
 
 import sys
 
-def climbing_stairs(n, cache=None):
-  pass 
+def climbing_stairs(n):
+  # base case
+  if n < 0:
+    return 0
 
+  if n == 0:
+    return 1
+
+  # this step moves towards our base case
+  else:
+    return climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
+
+print(climbing_stairs(3))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
