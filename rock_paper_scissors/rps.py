c1667eb9b124d1d95ea8ba6ad3e732672bba8907
CookieMonsta89 <jlstan06@hotmail.com>
2019-03-01 09:27:37 -0500

    finished rps sorry for all of the commits at once I finished last night so now im commit them one at a time

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,9 +3,20 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  outcomes = []
+  variations = ['rock', 'paper', 'scissors']
 
+  def find_winner(rounds_left, result=[]):
+    if rounds_left == 0:
+      outcomes.append(result)
+      return
+    for vari in variations:
+      find_winner(rounds_left -1, result + [vari])
 
+  find_winner(n, [])
+  return outcomes
+
+print(rock_paper_scissors(4))
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_plays = int(sys.argv[1])
